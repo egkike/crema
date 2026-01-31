abd294503db8d9b6c08cc30e2fa422b749f05b71
Kike Garcia <egkike@gmail.com>
2026-01-31 01:25:16 -0300

    Se corrige un import en payments.routes

--- backend/src/routes/payments.routes.ts
+++ backend/src/routes/payments.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
 import { jwtAuthMiddleware } from '../middlewares/auth/jwt.middleware';
-import { createPreference } from '../controllers/payment.controller';
+import { createPaymentPreference } from '../controllers/payment.controller';
 
 const router = Router();
 
 // Ruta protegida: solo usuarios autenticados pueden crear preferencia de pago
-router.post('/create-preference', jwtAuthMiddleware, createPreference);
+router.post('/create-preference', jwtAuthMiddleware, createPaymentPreference);
 
 export default router;
